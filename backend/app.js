330a2b9a3aeff2b4438c2e8f8069fead033ac175
daddyaezzy <pulumbu11@gmail.com>
2023-07-24 17:43:03 +0100

    da-video

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+// start up a node js server
+
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const cors = require("cors");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.POST || 5000;
+
+// app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.send("Server is running");
+});
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callended");
+  });
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+  });
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+});
+
+server.listen(PORT, function () {
+  console.log("listening on " + PORT);
+});
